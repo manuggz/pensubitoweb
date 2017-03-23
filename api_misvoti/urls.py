89e1f6e94cfaefd88620001ca553b75901326031
manuggz <manuelggonzalezm@gmail.com>
2017-03-23 12:03:35 -0400

    Cambiada vista principal de planes

--- api_misvoti/urls.py
+++ api_misvoti/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from api_misvoti import views
-from api_misvoti.views import UserDetail, UserList
+from api_misvoti.views import UserDetail, UserList#,UserPlanesCreadosList
 # from api_misvoti.views import UserPlanesCreadosList, UserDetail, UserList, PlanesCreadosDetail,PlanesCreadosTriList,TrimestresCreadosDetail, \
 #     PlanesCreadosList,TrimestresCreadosList
 
 user_urls = [
-    #url(r'^planes/(?P<pk>[0-9]+)', PlanesCreadosDetail.as_view(), name='planescreados-detail'),
+    #url(r'^(?P<username>[0-9a-zA-Z_-]+)/planes/$', UserPlanesCreadosList.as_view(), name='userplan-list'),
     url(r'^(?P<username>[0-9a-zA-Z_-]+)/$', UserDetail.as_view(), name='user-detail'),
     url(r'^$', UserList.as_view(), name='user-list')
 ]
