630be97c95048cba4874c2b3bce0fe42cdaf0d54
Manuel Gonzalez <manuelggonzalezm@gmail.com>
2017-03-24 13:08:05 -0400

    Quitados archivos con claves del github

--- misvoti/settings.py
+++ misvoti/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 import dj_database_url
+from base import SECRET_KEY,ADMINS
+from dev import DEBUG
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,12 +24,12 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wdx2dl$gpyjad7o%(l=!(qato_h26l7y^hjj@qtcx7l37jm4ao'
+# SECRET_KEY contenido en base.py
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG contenido en archivos dev.py y prod.py
 
-ADMINS = [('Manuel', 'manuelggonzalezm@gmail.com')]
+# ADMINS contenido en base.py
 
 AUTH_USER_MODEL = 'api_misvoti.MiVotiUser'
 
