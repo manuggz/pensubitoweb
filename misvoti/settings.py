c26bdd324ee99d486c3f2ad84d6f36a5dfa17a7e
Manuel Gonzalez <manuelggonzalezm@gmail.com>
2018-05-01 13:38:24 -0400

    Actualizado a python3.6 error en win con pipenv

--- misvoti/settings.py
+++ misvoti/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-import dj_database_url
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = 'wdx2dl$gpyjad7o%(l=!(qato_h26l7y^hjj@qtcx7l37jm4ao'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+ALLOWED_HOSTS = []
 ADMINS = [('Manuel', 'manuelggonzalezm@gmail.com')]
 
 AUTH_USER_MODEL = 'api_misvoti.MiVotiUser'
@@ -136,15 +137,9 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
@@ -161,3 +156,4 @@ STATICFILES_DIRS = [
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
